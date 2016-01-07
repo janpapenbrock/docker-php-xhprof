c934e4e191de5e5784bf4eb07eacb7e450b4bb7b
janpapenbrock <lunoxx@gmail.com>
2016-01-07 13:01:00 +0100

    Make code more robust against missing xhprof installation.

--- htdocs/xhprof-bootstrap.php
+++ htdocs/xhprof-bootstrap.php
@@ -1,14 +1,16 @@
 <?php
 
-xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+if (function_exists('xhprof_enable')) {
+    xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
 
-register_shutdown_function(function() {
-    $xhProfData = xhprof_disable();
-    include_once '/opt/xhprof/xhprof_lib/utils/xhprof_lib.php';
-    include_once '/opt/xhprof/xhprof_lib/utils/xhprof_runs.php';
+    register_shutdown_function(function() {
+        $xhProfData = xhprof_disable();
+        include_once '/opt/xhprof/xhprof_lib/utils/xhprof_lib.php';
+        include_once '/opt/xhprof/xhprof_lib/utils/xhprof_runs.php';
 
-    $xhProf = new XHProfRuns_Default();
-    $runId = time();
-    $runName = rand(10000000, 99999999);
-    $xhProf->save_run($xhProfData, $runName, $runId);
-});
+        $xhProf = new XHProfRuns_Default();
+        $runId = time();
+        $runName = rand(10000000, 99999999);
+        $xhProf->save_run($xhProfData, $runName, $runId);
+    });
+}
